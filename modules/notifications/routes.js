47da6aabfdf78583264e5c7e15fe754490201b81
Hager Shahin <hagershahin@Hagers-MBP.attlocal.net>
2025-11-22 17:46:18 -0500

    add notification features

--- modules/notifications/routes.js
+++ modules/notifications/routes.js
@@ -2,16 +2,16 @@
 const express = require("express");
 const router = express.Router();
 const notificationController = require("./controller");
-const { verifyCustomJwt } = require("../../middleware/verifyCustomJwt");
+const { verifyAnyToken } = require("../../middleware/verifyAnyTokens");
 
-router.post("/reminder", verifyCustomJwt, notificationController.sendAppointmentReminder);
-router.post("/promotion", verifyCustomJwt, notificationController.sendPromotionalOffer);
-router.post("/delay", verifyCustomJwt, notificationController.notifyClientDelay);
-router.post("/discount", verifyCustomJwt, notificationController.notifyUserDiscount);
+router.post("/reminder", verifyAnyToken, notificationController.sendAppointmentReminder);
+router.post("/promotion", verifyAnyToken, notificationController.sendPromotionalOffer);
+router.post("/delay", verifyAnyToken, notificationController.notifyClientDelay);
+router.post("/discount", verifyAnyToken, notificationController.notifyUserDiscount);
 
-router.get("/", verifyCustomJwt, notificationController.getUserNotifications);
-router.put("/:id/read", verifyCustomJwt, notificationController.markNotificationRead);
-router.put("/read-all", verifyCustomJwt, notificationController.markAllNotificationsRead);
+router.get("/", verifyAnyToken, notificationController.getUserNotifications);
+router.put("/:id/read", verifyAnyToken, notificationController.markNotificationRead);
+router.put("/read-all", verifyAnyToken, notificationController.markAllNotificationsRead);
 
 module.exports = router;
 
